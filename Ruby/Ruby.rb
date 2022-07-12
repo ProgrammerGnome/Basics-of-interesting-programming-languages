da0fa17357c5e2edc3aefefa3d8f11f588088cd3
Király Márk <69594158+TheLittleGnome@users.noreply.github.com>
2022-07-12 23:00:59 +0200

    Create Ruby.rb

---
+++ Ruby/Ruby.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+#-------------------------------- 1. PROGRAM --------------------------------
+
+def function()
+	variable_DNA = gets
+	chars = variable_DNA.split('')
+	chars.each { |c|
+		case c
+		when "A"
+		print "T"
+		when "T"
+		print "A"
+		when "G"
+		print "C"
+		when "C"
+		print "G"
+	    	end
+	}
+end
+function
+print "\n"
+
+
+#-------------------------------- 2. PROGRAM --------------------------------
