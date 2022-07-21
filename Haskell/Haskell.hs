bae32640ccd4a167c3cfb59171ba503ff9f59671
Király Márk <69594158+TheLittleGnome@users.noreply.github.com>
2022-07-21 20:32:10 +0200

    Create Haskell.hs

---
+++ Haskell/Haskell.hs
@@ -0,0 +1,9 @@
+------------------- Reverse list.md -------------------
+
+list_of_names = ["Alice", "Anna", "Tim", "Tom", "Austin"]
+reverseList = do
+	print (reverse list_of_names)
+main = do
+	reverseList
+  
+------------------- 2.Program -------------------
